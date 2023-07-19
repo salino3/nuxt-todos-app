3395e105bc6f30780c2fb805c5fe8da653d27980
salino3 <flaviobcn012@gmail.com>
2023-07-19 09:42:07 +0200

    first commit

---
+++ components/Task.vue
@@ -0,0 +1,27 @@
+<template>
+ <div :class="`task ${task.done ? 'is-complete' : ''}`">
+  <div class="content">
+    {{ task.content }}
+  </div>
+  <button @click="toggleDone">{{ task.done ? 'Undone' : 'Done' }}</button>
+  <button @click="removeTask" class="delete">Delete</button>
+ </div>
+</template>
+
+<script >
+export default {
+ props: ['task'],
+ methods: {
+  toggleDone(task) {
+    this.$store.commit('TOGGLE_TASK', this.task);
+  },
+  removeTask(task) {
+    this.$store.commit('REMOVE_TASK', this.task)
+  }
+ }
+}
+</script>
+
+<style lang="css" scope>
+
+</style>

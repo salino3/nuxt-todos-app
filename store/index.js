20a7068f45741b31b6c05401f25a07bbfe14b0b8
salino3 <flaviobcn012@gmail.com>
2023-07-19 10:06:51 +0200

    add sessioStorage

--- store/index.js
+++ store/index.js
@@ -1,16 +1,21 @@
 
 export const state = () => ({
-  tasks: []
-})
+  tasks: JSON.parse(sessionStorage.getItem("tasksData")) || [],
+});
 
 export const mutations = {
   ADD_TASK(state, task) {
     state.tasks = [{content: task, done: false}, ...state.tasks];
+    sessionStorage.setItem("tasksData", JSON.stringify(state.tasks));
   },
   REMOVE_TASK (state, task) {
     state.tasks.splice(state.tasks.indexOf(task), 1);
+    sessionStorage.setItem("tasksData", JSON.stringify(state.tasks));
   },
   TOGGLE_TASK(state, task) {
     task.done = !task.done;
+    sessionStorage.setItem("tasksData", JSON.stringify(state.tasks));
+
+
   }
 }
